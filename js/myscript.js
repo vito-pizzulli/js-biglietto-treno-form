295b3ff5195adf14b7a5be136fc8cc562c13d0f9
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-15 14:53:57 +0200

    milestone 1 completed, program implemented & running

--- js/myscript.js
+++ js/myscript.js
@@ -1,31 +1,42 @@
 /* Variables Declaring & Initializing */
-const kilometersNumber = parseInt(prompt("Inserisci il numero di km da percorrere:"));
-const passengerAge = parseInt(prompt("Inserisci l'età del passeggero:"));
+let kilometersNumber;
+let passengerAge;
+const button = document.querySelector('button.button');
+let ticketPrice;
 let ticketDiscount = 0;
 let ticketFinalPrice;
 let discountPercent = "0%";
 
-/* Discounts Calculation */
-let ticketPrice = (kilometersNumber * 0.21);
-if (passengerAge < 18) {
-    discountPercent = "20%";
-    ticketDiscount = ((20 / 100) * ticketPrice);
-    ticketFinalPrice = (ticketPrice - ticketDiscount);
-} else if (passengerAge > 65) {
-    discountPercent = "40%";
-    ticketDiscount = ((40 / 100) * ticketPrice);
-    ticketFinalPrice = (ticketPrice - ticketDiscount);
-} else {
-    ticketFinalPrice = ticketPrice;
-}
+/* Button Click Event - Processes the information entered in the two fields and performs the calculations at each new button click. */
+button.addEventListener('click',
+    function() {
+        kilometersNumber = parseInt(document.getElementById('kilometersNumber').value);
+        passengerAge = parseInt(document.getElementById('passengerAge').value);
 
-/* Output Values Transformation */
-    ticketPrice = ticketPrice.toFixed(2);
-    ticketDiscount = ticketDiscount.toFixed(2);
-    ticketFinalPrice = ticketFinalPrice.toFixed(2);
+        /* Discounts Calculation */
+        ticketPrice = (kilometersNumber * 0.233);
+        if (passengerAge < 18) {
+            discountPercent = "19,4%";
+            ticketDiscount = ((19.4 / 100) * ticketPrice);
+            ticketFinalPrice = (ticketPrice - ticketDiscount);
+        } else if (passengerAge > 65) {
+            discountPercent = "37,7%";
+            ticketDiscount = ((37.7 / 100) * ticketPrice);
+            ticketFinalPrice = (ticketPrice - ticketDiscount);
+        } else {
+            ticketFinalPrice = ticketPrice;
+        }
+
+        /* Output Values Transformation */
+        ticketPrice = ticketPrice.toFixed(2);
+        ticketDiscount = ticketDiscount.toFixed(2);
+        ticketFinalPrice = ticketFinalPrice.toFixed(2);
+
+        console.log(kilometersNumber, passengerAge, ticketPrice, ticketDiscount, discountPercent, ticketFinalPrice);
+    })
 
 /* Output - Only if the inserted infos are not NaN or 0 */
-if ((kilometersNumber == 0) || (passengerAge == 0) || isNaN(kilometersNumber) || isNaN(passengerAge)) {
+/* if ((kilometersNumber == 0) || (passengerAge == 0) || isNaN(kilometersNumber) || isNaN(passengerAge)) {
     document.getElementById("kilometersOutput").innerHTML = " "+ "Valori inseriti non validi!"
     document.getElementById("passengerAgeOutput").innerHTML = " " + "Valori inseriti non validi!"
     document.getElementById("priceOutput").innerHTML = " " + "Valori inseriti non validi!"
@@ -39,4 +50,4 @@ if ((kilometersNumber == 0) || (passengerAge == 0) || isNaN(kilometersNumber) ||
     document.getElementById("discountPercentOutput").innerHTML = " " + discountPercent;
     document.getElementById("discountPriceOutput").innerHTML = " " + ticketDiscount + " €";
     document.getElementById("finalPriceOutput").innerHTML = " " + ticketFinalPrice + " €";
-}
+} */
